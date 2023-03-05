e6ea5cb9de0d4a4736d6f58b193e073302ae1509
AreOlsen <are_olsen@hotmail.no>
2023-03-05 14:56:47 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content flex-col lg:flex-row-reverse gap-40">
+    <img src="tenksven.png" class="max-w-sm rounded-lg shadow-2xl" alt="Meme. Man says: I am people person. Goose says: I am a geese goose."/>
+    <div>
+      <h1 class="text-5xl font-bold">Are Olsen.</h1>
+      <p class="py-6">A young programmer from Norway.</p>
+      <button class="btn btn-primary">About Me</button>
+    </div>
+  </div>
+</div>
