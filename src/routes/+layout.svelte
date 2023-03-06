b75c6def1e3ce374b0af6df3d84c3632d1209324
SimonB <simonthedankone@gmail.com>
2023-03-06 19:09:49 +0100

    Update design and small stuff.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,12 @@
 	/*  
         TAILWINDCSS
     */
-	import '../app.css';
+	import "../app.css";
 	/* 
 		THEME CHANGE
 	*/
-	import { onMount } from 'svelte';
-	import { themeChange } from 'theme-change';
+	import { onMount } from "svelte";
+	import { themeChange } from "theme-change";
 	onMount(() => {
 		themeChange(false);
 	});
@@ -18,47 +18,115 @@
 </svelte:head>
 
 <header>
-    <div class="navbar bg-base-100">
-    <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl" href="/">Are Olsen</a>
-    </div>
-    <div class="flex-none">
-        <ul class="menu menu-horizontal px-1">
-        <li><a>About Me</a></li>
-        <li tabindex="0">
-            <a>
-            Projects
-            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-            </a>
-            <ul class="p-2 bg-base-100">
-            <li><a href="https://github.com/AreOlsen/FastFourierTransform">Fast Fourier Transform</a></li>
-            <li><a href="https://github.com/AreOlsen/Mandelbrot">Mandelbrot Set</a></li>
-            <li><a href="https://github.com/AreOlsen/Donut.CS">Donut</a></li>
-            <li><a href="https://github.com/AreOlsen?tab=repositories">More...</a></li>
-            </ul>
-        </li>
-        <li><a>Blog</a></li>
-        <li><a>Achievements</a></li>
-        <li><a>Contact Me</a></li>
-        </ul>
-    </div>
-    </div>
+	<div class="navbar bg-base-100">
+		<div class="flex-1">
+			<a class="btn btn-ghost normal-case text-xl" href="/">Are Olsen</a>
+		</div>
+		<div class="flex-none">
+			<ul class="menu menu-horizontal px-1">
+				<li><a href="/sites/aboutme">About Me</a></li>
+				<li tabindex="0">
+					<a>
+						Projects
+						<svg
+							class="fill-current"
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							viewBox="0 0 24 24"
+							><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg
+						>
+					</a>
+					<ul class="p-2 bg-base-100">
+						<li><a href="https://github.com/AreOlsen/FastFourierTransform">Fast Fourier Transform</a></li>
+						<li><a href="https://github.com/AreOlsen/Mandelbrot">Mandelbrot Set</a></li>
+						<li><a href="https://github.com/AreOlsen/Donut.CS">Donut</a></li>
+						<li><a href="https://github.com/AreOlsen?tab=repositories">More…</a></li>
+					</ul>
+				</li>
+				<li><a href="/sites/blog">Blog</a></li>
+				<li><a href="/sites/achievements">Achievements</a></li>
+				<li><a href="/sites/contactme">Contact Me</a></li>
+			</ul>
+		</div>
+	</div>
 </header>
-
 <slot />
-
 <footer class="footer footer-center p-10 bg-primary text-primary-content">
-  <div>
-    <img src="AO.png" width="75" height="75" alt="Primary Logo">
-    <p class="font-bold">
-      Are Olsen. <br/>An experienced programmer.
-    </p> 
-  </div> 
-  <div>
-    <div class="grid grid-flow-col gap-4">
-      <a href="https://github.com/AreOlsen"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="inline-block h-5 w-5 fill-current md:h-6 md:w-6"><path d="M256,32C132.3,32,32,134.9,32,261.7c0,101.5,64.2,187.5,153.2,217.9a17.56,17.56,0,0,0,3.8.4c8.3,0,11.5-6.1,11.5-11.4,0-5.5-.2-19.9-.3-39.1a102.4,102.4,0,0,1-22.6,2.7c-43.1,0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1,1.4-14.1h.1c22.5,2,34.3,23.8,34.3,23.8,11.2,19.6,26.2,25.1,39.6,25.1a63,63,0,0,0,25.6-6c2-14.8,7.8-24.9,14.2-30.7-49.7-5.8-102-25.5-102-113.5,0-25.1,8.7-45.6,23-61.6-2.3-5.8-10-29.2,2.2-60.8a18.64,18.64,0,0,1,5-.5c8.1,0,26.4,3.1,56.6,24.1a208.21,208.21,0,0,1,112.2,0c30.2-21,48.5-24.1,56.6-24.1a18.64,18.64,0,0,1,5,.5c12.2,31.6,4.5,55,2.2,60.8,14.3,16.1,23,36.6,23,61.6,0,88.2-52.4,107.6-102.3,113.3,8,7.1,15.2,21.1,15.2,42.5,0,30.7-.3,55.5-.3,63,0,5.4,3.1,11.5,11.4,11.5a19.35,19.35,0,0,0,4-.4C415.9,449.2,480,363.1,480,261.7,480,134.9,379.7,32,256,32Z"></path></svg></a> 
-      <a href="https://www.youtube.com/@ao_coding3670/featured"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
-      <a href="https://discord.gg/9snq8SfpZ9"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 127.14 96.36"><g id="图层_2" data-name="图层 2"><g id="Discord_Logos" data-name="Discord Logos"><g id="Discord_Logo_-_Large_-_White" data-name="Discord Logo - Large - White"><path d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z"/></g></g></g></svg></a>
-    </div>
-  </div>
+	<div>
+		<img src="/AO.png" width="75" height="75" alt="Primary Logo" />
+		<p class="font-bold">
+			Are Olsen. <br />An experienced programmer.
+		</p>
+	</div>
+	<div>
+		<div class="grid grid-flow-col gap-4">
+			<a href="https://github.com/AreOlsen"
+				><svg
+					width="24"
+					height="24"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 512 512"
+					class="inline-block h-5 w-5 fill-current md:h-6 md:w-6"
+					><path
+						d="M256,32C132.3,32,32,134.9,32,261.7c0,101.5,64.2,187.5,153.2,217.9a17.56,17.56,0,0,0,3.8.4c8.3,0,11.5-6.1,11.5-11.4,0-5.5-.2-19.9-.3-39.1a102.4,102.4,0,0,1-22.6,2.7c-43.1,0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1,1.4-14.1h.1c22.5,2,34.3,23.8,34.3,23.8,11.2,19.6,26.2,25.1,39.6,25.1a63,63,0,0,0,25.6-6c2-14.8,7.8-24.9,14.2-30.7-49.7-5.8-102-25.5-102-113.5,0-25.1,8.7-45.6,23-61.6-2.3-5.8-10-29.2,2.2-60.8a18.64,18.64,0,0,1,5-.5c8.1,0,26.4,3.1,56.6,24.1a208.21,208.21,0,0,1,112.2,0c30.2-21,48.5-24.1,56.6-24.1a18.64,18.64,0,0,1,5,.5c12.2,31.6,4.5,55,2.2,60.8,14.3,16.1,23,36.6,23,61.6,0,88.2-52.4,107.6-102.3,113.3,8,7.1,15.2,21.1,15.2,42.5,0,30.7-.3,55.5-.3,63,0,5.4,3.1,11.5,11.4,11.5a19.35,19.35,0,0,0,4-.4C415.9,449.2,480,363.1,480,261.7,480,134.9,379.7,32,256,32Z"
+					/></svg
+				></a
+			>
+			<a href="https://www.youtube.com/@ao_coding3670/featured"
+				><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"
+					><path
+						d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
+					/></svg
+				></a
+			>
+			<a href="https://discord.gg/9snq8SfpZ9"
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					class="fill-current"
+					viewBox="0 0 127.14 96.36"
+					><g id="图层_2" data-name="图层 2"
+						><g id="Discord_Logos" data-name="Discord Logos"
+							><g id="Discord_Logo_-_Large_-_White" data-name="Discord Logo - Large - White"
+								><path
+									d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z"
+								/></g
+							></g
+						></g
+					></svg
+				></a
+			>
+		</div>
+	</div>
 </footer>
+
+<style>
+	:root {
+		min-width: 100vw;
+		min-height: 100vh;
+	}
+
+	:global(body) {
+		min-width: 100%;
+		min-height: 100%;
+		margin: 0;
+		display: grid !important;
+		grid-template-columns: 100% !important;
+		grid-template-rows: auto minmax(auto, 95vh) auto !important;
+		grid-template-areas:
+			"header"
+			"center"
+			"footer" !important;
+	}
+	:global(header) {
+		grid-area: header !important;
+	}
+	:global(footer) {
+		grid-area: footer !important;
+	}
+	main {
+		grid-area: center !important;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
-<div class="hero min-h-screen bg-base-200">
-  <div class="hero-content flex-col lg:flex-row-reverse gap-40">
-    <img src="tenksven.png" class="max-w-sm rounded-lg shadow-2xl" alt="Meme. Man says: I am people person. Goose says: I am a geese goose."/>
-    <div>
-      <h1 class="text-5xl font-bold">Are Olsen.</h1>
-      <p class="py-6">A young programmer from Norway.</p>
-      <button class="btn btn-primary">About Me</button>
-    </div>
-  </div>
-</div>
+<main>
+	<div class="hero min-h-screen bg-base-200">
+		<div class="hero-content flex-col lg:flex-row-reverse gap-40">
+			<img
+				src="tenksven.png"
+				class="max-w-sm rounded-lg shadow-2xl"
+				alt="Meme. Man says: I am people person. Goose says: I am a geese goose."
+			/>
+			<div class="flex flex-col items-start">
+				<span class="text-2xl">Hi, my name is</span>
+				<h1 class="text-5xl font-bold">Are Olsen.</h1>
+				<span class="py-4 text-4xl ">I build systems and progress!</span>
+				<a href="/sites/aboutme" class="btn btn-primary">About Me</a>
+			</div>
+		</div>
+	</div>
+</main>
