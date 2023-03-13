6a8dbc663beada218b99bd8c8c24f35c26c73566
SimonB <simonthedankone@gmail.com>
2023-03-13 09:09:28 +0100

    Website is more or less finished!

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from "$app/stores";
+</script>
+
+<main class="flex-col gap-4">
+	<h1 class="text-5xl font-bold">{$page.status}</h1>
+	<h2 class="text-3xl italic">Error code: {$page.error.message}</h2>
+	<a class="btn btn-accent normal-case text-xl" href="/">Go To Main Page</a>
+</main>
--- src/routes/sites/blog/[slug]/+page.svelte
+++ src/routes/sites/blog/[slug]/+page.svelte
@@ -1,12 +1,14 @@
-<!-- src/routes/[slug]/+page.svelte -->
 <script>
-  export let data;
+	export let data;
 </script>
 
 <main>
-  <article class="w-9/12">
-    <h1 class="font-bold text-4xl">{ data.title }</h1>
-    <p class="italic text-2xl">Published: {data.date} by Are Olsen.</p> 
-    <svelte:component this={data.content} />
-  </article>
+	<article class="w-9/12 flex flex-col justify-center items-center gap-3">
+		<h1 class="font-bold text-5xl">{data.title}</h1>
+		<p class="italic text-2xl">Published on "{data.date}" by Are Olsen.</p>
+		<div class="divider" />
+		<div class="prose">
+			<svelte:component this={data.content} />
+		</div>
+	</article>
 </main>
